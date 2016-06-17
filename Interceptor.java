e07c84fc1375c6d752b0bbbe48dec20f88a03f67
Anthony Hewins <aahewins@oakland.edu>
2016-06-17 15:47:59 -0400

    Add files via upload

--- Interceptor.java
+++ Interceptor.java
@@ -1,14 +1,14 @@
 package com.example.aahewins.helloworld;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.widget.Toast;
-
 /**
  * Waits for an SMS message to be sent and prepares to send the SMS contents to the server
  * @author Anthony Hewins
@@ -16,47 +16,26 @@ import android.widget.Toast;
  * @since 1.0
  */
 public class Interceptor extends BroadcastReceiver {
-    BroadcastReceiver br = null; //The received broadcast to begin intercepting SMS_RECEIVED
-    /**
-     * Waits for Telephony.SMS_RECEIVED, then begins the onReceive method
-     * to begin reading SMS messages.
-     */
-    public void onCreate() {
-        IntentFilter i = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+    private String TAG = Interceptor.class.getSimpleName();
+    private String message;
 
-        br = new BroadcastReceiver() {
-          @Override
-            public void onReceive(Context c, Intent i) {
-              onReceive(c,i);
-          }
-        };
-    }
-    /**
-     * Begins parsing the incoming message and prepares to send it to the server.
-     * @param context The interceptor class starts the onReceive when ready
-     * @param intent Intent from the interceptor class to begin reading
-     */
-    @SuppressWarnings("deprecation") //Using createFromPdu(byte[],String) doesn't work for old APIs
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "text", Toast.LENGTH_LONG).show();
+    public Interceptor() {
+        }
 
-        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+        @SuppressWarnings("deprecation")
+        @Override
+        public void onReceive(Context context, Intent intent) {
             Bundle bundle = intent.getExtras();
             SmsMessage[] messages = null;
-            String from;
-            if (bundle != null) try {
+            if (bundle != null) {
                 Object[] pdus = (Object[]) bundle.get("pdus");
                 messages = new SmsMessage[pdus.length];
-                for (int i = 0; i < messages.length; i++) {
-                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]); //Deprecated
-                    String text = messages[i].getMessageBody();
+                for (int i=0; i < messages.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    this.message += messages[i].getMessageBody().toString();
                 }
-            } catch (Exception e) {
-                Log.d("Exception @Interceptor", "Exception @Interceptor");
-                e.printStackTrace();
-            }
+                Toast.makeText(context, "Extracted text message:\n"+message, Toast.LENGTH_LONG).show();
+//                new SendToServer(this.message);
         }
     }
 }
-
